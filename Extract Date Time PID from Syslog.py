015e7c7869bcfdf04460d27ff8687ce5352dc93b
Kena D <kenadudac@gmail.com>
2025-02-17 20:48:38 -0500

    Added script to extract date, time, and PID from syslog. Renamed 'Managing Data & Processes.py' to 'Input Function.py'.

---
+++ Extract Date Time PID from Syslog.py
@@ -0,0 +1,30 @@
+"""
+# TITLE: Extract Date, Time, and PID from Syslog
+
+# Created by: Kena D
+# Created on: 2025-02-16 
+# Last editted on: 2025-02-16
+
+"""
+
+#========================================================
+
+import re
+def show_time_of_pid(line):
+  pattern = r'^(\w+ \d+ \d+:\d+:\d+) .*?\[(\d+)\]'
+  result = re.search(pattern, line)
+  return f"{result.group(1)} pid:{result.group(2)}"
+
+print(show_time_of_pid("Jul 6 14:01:23 computer.name CRON[29440]: USER (good_user)")) # Jul 6 14:01:23 pid:29440
+
+print(show_time_of_pid("Jul 6 14:02:08 computer.name jam_tag=psim[29187]: (UUID:006)")) # Jul 6 14:02:08 pid:29187
+
+print(show_time_of_pid("Jul 6 14:02:09 computer.name jam_tag=psim[29187]: (UUID:007)")) # Jul 6 14:02:09 pid:29187
+
+print(show_time_of_pid("Jul 6 14:03:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:03:01 pid:29440
+
+print(show_time_of_pid("Jul 6 14:03:40 computer.name cacheclient[29807]: start syncing from \"0xDEADBEEF\"")) # Jul 6 14:03:40 pid:29807
+
+print(show_time_of_pid("Jul 6 14:04:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:04:01 pid:29440
+
+print(show_time_of_pid("Jul 6 14:05:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:05:01 pid:29440
---
+++ Input Function.py
@@ -0,0 +1,27 @@
+"""
+# TITLE: Input Function  
+
+# Created by: Kena D
+# Created on: 2025-02-16 
+# Last editted on: 2025-02-16
+
+"""
+
+#========================================================
+
+def to_seconds(hours, minutes, seconds):
+    return hours*3600+minutes*60+seconds
+
+print("Welcome to this time converter")
+
+cont = "y"
+while(cont.lower() == "y"):
+    hours = int(input("Enter the number of hours: "))
+    minutes = int(input("Enter the number of minutes: "))
+    seconds = int(input("Enter the number of seconds: "))
+
+    print("That's {} seconds".format(to_seconds(hours, minutes, seconds)))
+    print()
+    cont = input("Do you want to do another conversion? [y to continue] ")
+    
+print("Goodbye!")
